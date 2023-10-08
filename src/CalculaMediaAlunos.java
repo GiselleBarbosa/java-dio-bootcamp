64a4c29a0bfad83e0866a9d01af37315df31b039
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-08 01:03:14 -0300

    exemplo - calcula media aluno

---
+++ src/CalculaMediaAlunos.java
@@ -0,0 +1,14 @@
+public class CalculaMediaAlunos {
+    public static void main(String[] args) {
+        int mediaFinal = 3;
+
+        if (mediaFinal < 6) {
+            System.out.println("Aluno(a) reprovado(a)");
+        } else if (mediaFinal == 6) {
+            System.out.println("Aluno(a) irá para recuperação");
+
+        } else {
+            System.out.println("Aluno(a) Aprovado(a)");
+        }
+    }
+}
