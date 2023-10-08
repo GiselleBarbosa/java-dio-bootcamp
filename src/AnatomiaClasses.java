356e3e71c7ee77e947fe07156d280aa7dc20a920
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-08 00:22:09 -0300

    outros exemplos de nomenclatura de variaveis

---
+++ src/AnatomiaClasses.java
@@ -0,0 +1,22 @@
+public class AnatomiaClasses {
+    public static void main(String[] args) {
+        // Estrutura
+        //
+        // Tipo NomeBemDefinido = (Atribuição opcional em alguns casos)
+
+        // Exemplo
+
+        String meuNome = "Giselle";
+        int idade = 34;
+        boolean temFilhos = false;
+
+        if (temFilhos) {
+            System.out.println("Bem vindo(a) " + meuNome + " Sua idade é " + idade + " e voce possui filho(s).");
+        } else {
+            System.out.println("Bem vindo(a) " + meuNome + " Sua idade é " + idade + " e voce NAO possui filho(s).");
+
+        }
+
+
+    }
+}
