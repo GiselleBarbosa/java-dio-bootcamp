4c3cb8fb195ca4fdbf8ea45a71f6e38e58988805
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-08 00:03:59 -0300

    exemplos de variaveis

--- src/JavaHelloWorld.java
+++ src/JavaHelloWorld.java
@@ -1,23 +1,5 @@
 public class JavaHelloWorld {
     public static void main(String[] args) {
-
-        // Final se assemelha a CONST - Constant em javascript
-        final String BR = "Brasil";
-        // BR = "Brazil";
-        // Utilizando final a variavel BR não pode ser alterada
-        // Declarar as variaveis FINAL utilizando letras MAIUSCULAS
-        // Separar nomes por underline
-
-        System.out.println(BR); // Brasil
-
-        // ------------------------------------------------------------
-
-        // Declarada variaval ano valendo 2000
-        int ano = 2000;
-        System.out.println(ano);// 2000
-
-        // Houve alteração do valor para 2023
-        ano = 2023;
-        System.out.println(ano); // 2023
+        System.out.print("Hello World, Giselle!!!");
     }
 }
