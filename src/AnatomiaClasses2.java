77da5d89581f0420143f96677d6c9e16b49f5c1e
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-08 00:58:13 -0300

    exemplo utilizando concat.

---
+++ src/AnatomiaClasses2.java
@@ -0,0 +1,16 @@
+public class AnatomiaClasses2 {
+
+    public static void main(String[] args) {
+        String primeiroNome = "Giselle";
+        String sobreNome = "Barbosa";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, sobreNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String sobreNome) {
+        return "Nome completo é: " + primeiroNome.concat(" ").concat(sobreNome);
+    }
+
+
+}
