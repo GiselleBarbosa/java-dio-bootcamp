0d9124bf63f5526aabf24302b77c7fdc357efc79
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-09 00:36:41 -0300

    explicacao sobre escopo

---
+++ src/bootcamp/giselle/escopo/Escopo.java
@@ -0,0 +1,9 @@
+package bootcamp.giselle.escopo;
+
+public class Escopo {
+    // ESCOPO
+
+    // Escopo é o ambiente onde uma variavel pode ser acessada.
+    // Ao declarar uma variavel dentro de um metodo ela so sera visivel dentro dele.
+    // Ao declarar a nivel de classe a variavel fica disponivel em escopo global
+}
