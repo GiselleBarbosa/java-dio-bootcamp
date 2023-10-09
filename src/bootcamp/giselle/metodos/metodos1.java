4acada15fbe81d176240aad57788b3dd79981690
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-08 23:44:40 -0300

    criterios de nomenclatura de metodos

---
+++ src/bootcamp/giselle/metodos/metodos1.java
@@ -0,0 +1,16 @@
+package bootcamp.giselle.metodos;
+public class metodos1 {
+    // CRITERIOS DE NOMEACAO DE METODOS - CONVECÕES
+
+    /*  - deve ser nomeado como verbo
+        - deve seguir o camelCase (primeira letra minuscula e demais palavras comecam com letra maiuscula)
+        - os metodos serao sempre definidos dentro de uma classe - nao existem metodos globais
+        - entender qual sera a proposta do metodo
+        - analisar se o metodo devera ou nao ter algum retorno
+        - caso nao retorne valor sera declarado como VOID
+        - visibilidade do metodo - private public e protected
+        (public - toda aplicacao, private mesmo pacote ou protected heranca ou somente a nivel da propria classe)
+        - o metodo possui risco de apresentar alguma exceção? Se sim, necessario prever e fazer o tratamento.
+        - verificar se havera necessidade de parametros
+    */
+}
