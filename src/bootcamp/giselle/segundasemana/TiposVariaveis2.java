355027638f0eb62149a75a9d99e2030d9b6c78f6
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-08 21:41:09 -0300

    constantes em java - FINAL

---
+++ src/bootcamp/giselle/segundasemana/TiposVariaveis2.java
@@ -0,0 +1,11 @@
+package bootcamp.giselle.segundasemana;
+
+public class TiposVariaveis2 {
+    public static void main(String[] args) {
+        double salarioMinimo = 1400;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // cast = converteu para um tipo mais específico
+    }
+}
