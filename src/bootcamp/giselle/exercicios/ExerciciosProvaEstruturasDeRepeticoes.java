b65b7e51e599db07a000dec1ec7a0c1bef896d96
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-17 11:01:19 -0300

    avaliacao

---
+++ src/bootcamp/giselle/exercicios/ExerciciosProvaEstruturasDeRepeticoes.java
@@ -0,0 +1,51 @@
+package bootcamp.giselle.exercicios;
+
+public class ExerciciosProvaEstruturasDeRepeticoes {
+  public static void main(String[] args) {
+    /*
+     * int numero = 1;
+     * 
+     * for (int x = 1; x < 2; x++) {
+     * numero = numero + x;
+     * }
+     * System.out.println("O valor de número é: " + numero);
+     * }
+     */
+
+    // O valor de número é: 2
+
+    /*
+     * public static void main(String[] args) {
+     * for (int x = 1; x <= 10; x++) {
+     * if (x % 2 == 0) // % operador que representa módulo de uma divisão
+     * System.out.println(x);
+     * }
+     */
+
+    // 2,4,6,8,10
+
+    /*
+     * int num = 5, count = 1;
+     * do {
+     * num += count;
+     * System.out.println(num);
+     * } while (count <= 3);
+     */
+
+    // Entra em Looping Infinito
+
+    /*
+     * boolean condicao = false;
+     * 
+     * while (condicao) {
+     * System.out.println("executou ...");
+     * }
+     */
+
+    // Não exibe nada
+
+    String[] nomes = { "Camila", "Venilton", "Leonardo", "Renan", "Rafael" };
+    System.out.print(nomes.length);
+  }
+
+}
