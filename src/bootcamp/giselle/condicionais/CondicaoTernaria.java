5b9b1165bb941226db36b47bf678eda0ede8659b
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-16 22:35:12 -0300

    adicionado exemplo de condicao ternaria

---
+++ src/bootcamp/giselle/condicionais/CondicaoTernaria.java
@@ -0,0 +1,12 @@
+package bootcamp.giselle.condicionais;
+
+public class CondicaoTernaria {
+  public static void main(String[] args) {
+    int nota = 7;
+
+    String resultado = nota >= 7 ? "Aluno APROVADO" : "Aluno REPROVADO";
+
+    System.out.println(resultado);
+
+  }
+} 
