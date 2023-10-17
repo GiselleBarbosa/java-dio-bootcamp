c6f4b2fca7c62c31fcbcd4586a055ee995bf1b57
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-17 09:41:46 -0300

    adicionados exemplos forEach e break/continue

---
+++ src/bootcamp/giselle/estruturas_de_repeticao/ExemploForEach.java
@@ -0,0 +1,12 @@
+package bootcamp.giselle.estruturas_de_repeticao;
+
+public class ExemploForEach {
+  public static void main(String[] args) {
+    String alunos[] = { "GISELLE", "YAGO", "TIAGO", "MARCOS" };
+
+    for (String aluno : alunos) {
+      System.out.println("O none do aluno é : " + aluno);
+    }
+  }
+
+}
